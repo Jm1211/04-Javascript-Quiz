af0683336368a28093b4ff1af1d11ab5798d9903
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2022-10-09 18:01:20 -0600

    add javascript for questions

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+var questions=[
+    {
+        text: "Commonly used data types DO NOT include:",
+        choices: ["strings","booleans","alerts","numbers"],
+        answer: "alerts"
+    },
+    {
+        text: "The condition in an if/else statement is enclosed with:",
+        choices: ["quotes","curly brackets","parenthesis","square brackets"],
+        answer: "parenthesis"
+    },
+    {
+        text: "Arrays in Javascript can be used to store:",
+        choices: ["numbers and strings","other arrays","booleans","all of the above"],
+        answer: "all of the above"
+    },
+    {
+        text: "A useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript","terminal/bash","for loop","console.log"],
+        answer: "console.log"
+    }
+
+
+];
